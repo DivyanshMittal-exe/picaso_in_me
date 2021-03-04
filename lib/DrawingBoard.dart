f5705722343c94901bf4780731677b44f7d161e5
Divyansh Mittal <divyanshmittal37@gmail.com>
2021-03-04 20:36:10 +0530

    Completely changed the method of input from text box based to PopUp based

---
+++ lib/DrawingBoard.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DrawBoard extends StatefulWidget {
+  @override
+  _DrawBoardState createState() => _DrawBoardState();
+}
+
+class _DrawBoardState extends State<DrawBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+
--- lib/picaso.dart
+++ lib/picaso.dart
@@ -1,12 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 class Drawing{
   String title="";
   Drawing(String title){
     this.title=title;
-
   }
 
 }
+
+int count = 2;
+
 List<Widget> getWidgetList(List<Drawing> listItems){
   List<ListTile> widgets = [];
   for(int i=0;i<listItems.length;i++){
@@ -20,6 +24,10 @@ String val="";
 
 
 
+
+
+
+
 class PicasoScreen extends StatefulWidget {
   @override
   _PicasoScreenState createState() => _PicasoScreenState();
@@ -27,7 +35,7 @@ class PicasoScreen extends StatefulWidget {
 
 class _PicasoScreenState extends State<PicasoScreen> {
 
-  List<Drawing> listItems =[Drawing("Sample 1")];
+  List<Drawing> listItems =[Drawing("Sample 1"),Drawing("Sample 2"),Drawing("Sample 3")];
 
   void addDrawing(String title){
     setState(() {
@@ -36,31 +44,85 @@ class _PicasoScreenState extends State<PicasoScreen> {
 
   }
 
+
+
+  TextEditingController _textFieldController = TextEditingController();
+
+  Future<void> _displayTextInputDialog(BuildContext context) async {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('TextField in Dialog'),
+          content: TextField(
+            controller: _textFieldController,
+            decoration: InputDecoration(hintText: "Text Field in Dialog"),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('CANCEL'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            FlatButton(
+              child: Text('OK'),
+              onPressed: () {
+                addDrawing(_textFieldController.text);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Picaso In Me"),
       ),
-      body: ListView(
-        children:<Widget>  [
-          TextField(
-            onChanged: (String temp) {
-              val = temp;
-            }
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.separated(
+          itemCount: listItems.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(listItems[index].title),
+            );
+          },
 
-          )
-          //getWidgetList(listItems),
-        ] + getWidgetList(listItems)
+          separatorBuilder: (context, index){
+
+            return Divider();
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        tooltip: "Add new drawing",
         child: Icon(Icons.add),
-        onPressed:(){
-          print("Pressed");
-          addDrawing(val);
-        }
+         onPressed: () { _displayTextInputDialog(context); },
       ),
     );
+
   }
+
+
+  void addItem(){
+
+    setState(() {
+      listItems.add(listItems[count]);
+    });
+
+  }
+
 }
+
