e56867d89501f92fbca51c91f9d5c52d54c833a1
Divyansh Mittal <divyanshmittal37@gmail.com>
2021-03-05 11:44:45 +0530

    Added the option of swipe to Dismiss

--- lib/picaso.dart
+++ lib/picaso.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:picaso_in_me/DrawingBoard.dart';
+import 'package:flutter/foundation.dart';
 
 
 class Drawing extends StatelessWidget {
@@ -53,7 +54,7 @@ class PicasoScreen extends StatefulWidget {
 
 class _PicasoScreenState extends State<PicasoScreen> {
 
-  List<Drawing> listItems =[Drawing("Sample 1"),Drawing("Sample 2"),Drawing("Sample 3")];
+  List<Drawing> listItems =[Drawing("The Old Guitarist"),Drawing("Girl Before a Mirror"),Drawing("The Weeping Woman")];
 
   void addDrawing(String title){
     setState(() {
@@ -114,17 +115,43 @@ class _PicasoScreenState extends State<PicasoScreen> {
         child: ListView.separated(
           itemCount: listItems.length,
           itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(listItems[index].title),
-                onTap: (){
-                print("Tapped");
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) =>DrawBoard()),
-                );
+            final item = listItems[index].title;
+            return Dismissible(
+              // Each Dismissible must contain a Key. Keys allow Flutter to
+              // uniquely identify widgets.
+                key: Key(item),
+                // Provide a function that tells the app
+                // what to do after an item has been swiped away.
+                onDismissed: (direction) {
+                  // Remove the item from the data source.
+                  setState(() {
+                    listItems.removeAt(index);
+                  });
 
-              }
+                  // Then show a snackbar.
+                 // ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("$item dismissed")));
+                },
+                // Show a red background as the item is swiped away.
+                background: Container(color: Colors.red),
+
+
+
+
+              child:  ListTile(
+                       title: Text(listItems[index].title),
+                       onTap: (){
+                      print("Tapped");
+                      Navigator.push(
+                      context,
+                        MaterialPageRoute(builder: (context) =>DrawBoard()),);
+
+            }
+              )
             );
+
+
+
+
           },
 
           separatorBuilder: (context, index){
